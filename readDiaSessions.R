e32ea3cc091e4972f4620b5ace9f16b066b46d9f
Sun Jay Yoo <sunjayyoo@Sun-Jays-MacBook.local>
2017-06-23 14:58:26 -0400

    Read Diatrack session file function completed.

--- readDiaSessions.R
+++ readDiaSessions.R
@@ -2,50 +2,76 @@
 install.packages("R.matlab")
 library(R.matlab)
 
-
-#Load in file
-file <- '/Users/sunjayyoo/Dropbox/Work/Diatrack\ Export/HTZ1Halo_fr10ms_120mW_14.mat'
-data <- readMat(file)$tracks
-backup <- data
-temp <- data[1][[1]][[1]][[7]]
-data[1][[1]][[1]][[7]] <- data[1][[1]][[1]][[6]]
-data[1][[1]][[1]][[6]] <- temp
-#data[FRAME][[1]][[1]][[ROWVARIABLE]][[VALUE]]
-
-#Start of algorithm
-startIndex = 0;
-startFrame = 1;
-trajectories = 1;
-track.list = list();
-repeat{
+#readDiaSessions function definition
+readDiaSessions = function(file, interact = FALSE, timer = FALSE){
+    
+    # interactively open window
+    if (interact == TRUE) {
+        file=file.choose();
+    }
+    if (timer == TRUE) {
+        start.time = Sys.time()
+    }
+    
+    file.name = basename(file);
+    cat("\nReading Diatrack session file: ",file.name,"...\n");
+    
+    #Pre-process data
+    data <- readMat(file)$tracks;
+    #backup <- data
+    temp <- data[1][[1]][[1]][[7]];
+    data[1][[1]][[1]][[7]] <- data[1][[1]][[1]][[6]];
+    data[1][[1]][[1]][[6]] <- temp;
+    #data[FRAME][[1]][[1]][[ROWVARIABLE]][[VALUE]]
+    
+    #Start of algorithm
+    startIndex = 0;
+    startFrame = 1;
+    trajectoryIndex = 1;
+    track.list = list();
     repeat{
-        if (startIndex == 0 || startIndex < ncol(data[[startFrame]][[1]][[1]])){
-            startIndex = startIndex + 1;
-        } else {
-            startFrame = startFrame + 1;
-            startIndex = 1;
+        repeat{
+            if (startIndex == 0 || startIndex < ncol(data[[startFrame]][[1]][[1]])){
+                startIndex = startIndex + 1;
+            } else {
+                startFrame = startFrame + 1;
+                startIndex = 1;
+            }
+            if (startFrame > length(data)){
+                break;
+            } else if (length(data[startFrame][[1]][[1]][[1]]) == 0){
+                startFrame = startFrame + 1;
+                startIndex = 1;
+            } else if (data[startFrame][[1]][[1]][[6]][[startIndex]] == 0)
+                break;
         }
-        if (startFrame > length(data)){
-            break
-        } else if (length(data[startFrame][[1]][[1]][[1]]) == 0){
-            startFrame = startFrame + 1;
-            startIndex = 1;
-        } else if (data[startFrame][[1]][[1]][[6]][[startIndex]] == 0)
-            break
+        if (startFrame > length(data))
+            break;
+        frame = startFrame;
+        index = startIndex;
+        track <- data.frame(x = numeric(), y = numeric(), z = integer());
+        repeat{
+            RefinedCooX = round(data[frame][[1]][[1]][[2]][[index]], 2);
+            RefinedCooY = round(data[frame][[1]][[1]][[1]][[index]], 2);
+            RefinedCooZ = round(data[frame][[1]][[1]][[3]][[index]], digits = 1);
+            track <- rbind(track, data.frame(x = RefinedCooX, y = RefinedCooY, z = RefinedCooZ));
+            if (data[frame][[1]][[1]][[7]][[index]] != 0){
+                index = data[frame][[1]][[1]][[7]][[index]];
+                frame = frame + 1;
+            } else
+                break;
+        }
+        track.list[[trajectoryIndex]] <- track;
+        trajectoryIndex = trajectoryIndex + 1;
     }
-    if (startFrame > length(data))
-        break
-    frame = startFrame;
-    index = startIndex;
-    track <- data.frame(x = numeric(), y = numeric(), z = integer());
-    repeat{
-        track <- rbind(track, data.frame(x = data[frame][[1]][[1]][[2]][[index]], y = data[frame][[1]][[1]][[1]][[index]], z= data[frame][[1]][[1]][[3]][[index]]))
-        if (data[frame][[1]][[1]][[7]][[index]] != 0){
-            index = data[frame][[1]][[1]][[7]][[index]];
-            frame = frame + 1;
-        } else
-            break
+    if (timer == TRUE) {
+        end.time = Sys.time();
+        time.taken = end.time - start.time;
+        cat("\nDuration: ");
+        cat(time.taken);
+        cat(" mins\n");
     }
-    track.list[[trajectories]] = track
-    trajectories = trajectories + 1
-}
+    cat("Session file read.\n")
+    return(track.list);
+} 
+
