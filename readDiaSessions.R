f823e3de91adc0f1235cc06a6196473b9c8fe925
Sun Jay Yoo <sunjayyoo@Sun-Jays-MacBook.local>
2017-07-11 14:20:51 -0400

    Parallel processing feature added and integrated into smt pipeline.

--- readDiaSessions.R
+++ readDiaSessions.R
@@ -1,7 +1,7 @@
 #### readDiaSessions.R
 #### Wu Lab, Johns Hopkins University
 #### Author: Sun Jay Yoo
-#### Date: July 6, 2017
+#### Date: July 10, 2017
 
 ## readDiaSessions-methods
 ##
@@ -16,22 +16,16 @@
 ##' @description take in a Diatrack .mat session file as input, along with several other user-configurable parameters and output options, to return a track list of all the trajectories found in the session file
                                          
 ##' @usage 
-##' readDiaSessions(file, interact = TRUE, censorSingle = TRUE, frameRecord = TRUE, rowWise = FALSE, colWise = FALSE, timer = FALSE)
+##' .readDiaSessions(file, interact = T, ab.track = F, censorSingle = T, frameRecord = T)
 ##'
 ##' removeFrameRecord(track.list)
 ##' 
-##' outputRowWise(track.list)
-##' 
-##' outputColWise(track.list)
 
 ##' @param file Full path to Diatrack .mat session file
 ##' @param interact Open menu to interactively choose file
+##' @param ab.track Use absolute coordinates for tracks
 ##' @param censorSingle Remove and censor trajectories that do not have a recorded next/previous frame (trajectories that appear for only one frame)
 ##' @param frameRecord add a fourth column to the track list after the xyz-coordinates for the frame that coordinate point was found (especially helpful when linking frames)
-##' @param rowWise Output .csv file in current directory of tracks in row-wise (ImageJ style) organization using outputRowWise function call
-##' @param colWise Output .csv file in current directory of tracks in column-wise (Diatrack stye) using outputColWise function call
-##' @param timer Time the computation duration of the script
-##' @param track.list A track list (a list of trajectory data frames)
 
 ##' @details
 ##' The naming scheme for each track is as follows:
@@ -43,33 +37,29 @@
 ##' removeFrameRecord is a helper script aims to make track lists with a fourth frame record column backwards compatible 
 ##' with other smt functions that rely on track lists with only three columns for the xyz-coordinates. 
 ##' The fourth column is simply removed from the given track list.
-##' 
-##' outputRowWise is only compatible with track lists with the fourth frame record column.
-##' 
-##' outputColWise is compatible with track lists both with or without the fourth frame record column.
+##'
 
 ##' @examples
-##' #Basic function call of readDiaSessions
-##' trackll <- readDiaSessions()
-##' 
-##' #Function call of readDiaSessions without a frame record and output to .csv files
-##' trackll2 <- readDiaSessions(frameRecord = F, rowWise = T, colWise = T)
+##' #Basic function call of .readDiaSessions
+##' trackl <- .readDiaSessions(interact = T)
+##'
+##' #Function call of .readDiaSessions with censoring without a frame record
+##' trackl2 <- .readDiaSessions(interact = T, censorSingle = T, frameRecord = F)
 ##' 
 ##' #Option to output .csv files after processing the track lists
-##' outputRowWise(trackll)
-##' outputColWise(trackll)
+##' .exportRowWise(trackl)
+##' .exportColWise(trackl)
 ##' 
 ##' #To find your current working directory
 ##' getwd()
 ##' 
 ##' #Remove default fourth frame record column
-##' trackll.removed <- removeFrameRecord(trackll)
+##' trackll.removed <- removeFrameRecord(trackl)
 ##' 
 
-##' @export readDiaSessions
+##' @export .readDiaSessions
 ##' @export removeFrameRecord
-##' @export outputColWise
-##' @export outputRowWise
+##' @export readDiaSessions
 
 ##' @importFrom R.matlab readMat
 
@@ -92,7 +82,6 @@
 
 #Using readDiaSessions, the intermediate .txt file was no longer needed to be created and the session file directly results in track lists.
 #This script resulted in 4487 censored tracks in 2:00 mins and 34689 uncensored tracks in 2:01 mins. 
-#All function inputs, except the timer and interact, was set as FALSE
 
 #### readDiaSessions ####
 
@@ -100,7 +89,7 @@
 #install.packages("R.matlab")
 #library(R.matlab)
 
-readDiaSessions = function(file, interact = TRUE, censorSingle = TRUE, frameRecord = TRUE, rowWise = FALSE, colWise = FALSE, timer = FALSE){
+.readDiaSessions = function(file, interact = F, ab.track = F, censorSingle = F, frameRecord = T){
     
     #Interactively open window
     if (interact == TRUE) {
@@ -108,9 +97,9 @@ readDiaSessions = function(file, interact = TRUE, censorSingle = TRUE, frameReco
     }
     
     #Start timer
-    if (timer == TRUE) {
-        start.time = Sys.time()
-    }
+    #if (timer == TRUE) {
+    #    start.time = Sys.time()
+    #}
     
     #Collect file name information
     file.name = basename(file);
@@ -204,6 +193,11 @@ readDiaSessions = function(file, interact = TRUE, censorSingle = TRUE, frameReco
             #Add track length to length list
             length.list[[length(length.list) + 1]] <- nrow(track);
             
+            #Calcualte absolute track coordinates if desired
+            if (ab.track){
+                track <- abTrack(track);
+            }
+            
             #Append temporary track for particle into track list and iterate to the next trajectory
             track.list[[trajectoryIndex]] <- track;
             trajectoryIndex = trajectoryIndex + 1;
@@ -214,26 +208,16 @@ readDiaSessions = function(file, interact = TRUE, censorSingle = TRUE, frameReco
     names(track.list) = paste(file.subname, frame.list, length.list, c(1:length(track.list)), sep=".");
     
     #File read and processedconfirmation text
-    cat("Session file read and processed.\n\n")
-    
-    #Return ImageJ style row-wise output with trajectory and frame numbers in .csv file
-    if (rowWise) { 
-        outputRowWise(track.list);
-    } 
-    
-    #Return column-wise output in .csv file
-    if (colWise) {
-        outputColWise(track.list);
-    } 
+    cat("\nSession file read and processed.\n")
     
     #Display stop timer
-    if (timer == TRUE) {
-        end.time = Sys.time();
-        time.taken = end.time - start.time;
-        cat("Duration: ");
-        cat(time.taken);
-        cat(" mins\n");
-    }
+    #if (timer == TRUE) {
+    #    end.time = Sys.time();
+    #    time.taken = end.time - start.time;
+    #   cat("Duration: ");
+    #    cat(time.taken);
+    #    cat(" mins\n");
+    #}
     
     #Return track list
     return(track.list);
@@ -248,61 +232,184 @@ removeFrameRecord = function(track.list){
     return (track.list);
 }
 
-#### outputRowWise ####
+#### readDiaSessions ####
 
-#MUST USE TRACK LIST WITH FRAME RECORD
-
-outputRowWise = function(track.list){
+readDiaSessions = function(folder, merge = F, ab.track = F, mask = F, cores = 1, censorSingle = T, frameRecord = T){
     
-    #Confirmation text of function call
-    cat("Writing .csv row-wise output in current directory...\n");
+    trackll = list()
+    track.holder = c()
     
-    #Empty data frame df to be written into the .csv
-    df <- NULL;
+    # getting a file list of Diatrack files in a directory
+    file.list = list.files(path = folder, pattern = ".mat", full.names = T)
+    file.name = list.files(path = folder, pattern = ".mat", full.names = F)
+    folder.name=basename(folder)
     
-    #Loop through every trajectory in input track.list
-    for (i in 1:length(track.list)){
+    # read in mask
+    #mask.list=list.files(path=folder,pattern="_MASK.tif",full.names=T)
+    
+    #if (mask==T & length(mask.list)==0){
+    #    cat("No image mask file ending '_MASK.tif' found.\n")
         
-        #Create a data frame temp with trajectory, frame, and track coordinate data 
-        temp <- data.frame(Trajectory = i, Frame = track.list[[i]][4], track.list[[i]][1:3]);
+    #}
+    
+    
+    # read in tracks
+    # list of list of data.frames,
+    # first level list of file names and
+    # second level list of data.frames
+    
+    max.cores = parallel::detectCores(logical = F)
+    
+    if (cores == 1){
         
-        #Append data frame df with data frame temp
-        df <- rbind(df, temp);
-    }
-    
-    #Write the data frame df into the .csv and display confirmation text
-    write.csv(df, file="outputRow.csv");
-    cat("outputRow.csv placed in current directory.\n\n");
-}
-
-#### outputColWise ####
-
-#Install packages and dependencies
-#library(plyr)
-
-outputColWise = function(track.list){
-    
-    #Confirmation text of function call
-    cat("Writing .csv column-wise output in home directory...\n");
-    
-    #Empty data frame df to be written into the .csv
-    df <- NULL;
-    
-    #Loop through every trajectory in input track.list
-    for (i in 1:length(track.list)){
-        
-        #Create temporary data frame to be filled with transposed lists from track.list
-        temp <- NULL;
-        for (j in 1:3){
-            var <- data.frame(t(track.list[[i]][j]));
-            temp <- rbind(temp, var);
+        for (i in 1:length(file.list)){
+            
+            track.list = .readDiaSessions(file = file.list[i], ab.track = ab.track, censorSingle = censorSingle, frameRecord = frameRecord)
+            
+            # add indexPerTrackll to track name
+            indexPerTrackll = 1:length(track.list)
+            names(track.list) = mapply(paste, names(track.list), indexPerTrackll,sep = ".")
+            
+            trackll[[i]] = track.list
+            names(trackll)[i] = file.name[i]
         }
         
-        #Append data frame df for .csv with temporary data frame
-        df <- rbind.fill(df, temp);
+    } else {
+        
+        # parallel this block of code
+        # assign reading in using .readDiatrack to each CPUs
+        
+        # detect number of cores
+        # FUTURE: if more than one, automatic using multicore
+        
+        if (cores>max.cores)
+            stop("Number of cores specified is greater than recomended maximum: ", max.cores)
+        
+        cat("Initiated parallel execution on", cores, "cores\n")
+        # use outfile="" to display result on screen
+        cl <- parallel::makeCluster(spec = cores,type = "PSOCK", outfile = "")
+        # register cluster
+        parallel::setDefaultCluster(cl)
+        
+        # pass environment variables to workers
+        parallel::clusterExport(cl,varlist=c(".readDiaSessions","ab.track", "censorSingle", "frameRecord"),envir=environment())
+        
+        # trackll=parallel::parLapply(cl,file.list,function(fname){
+        trackll=parallel::parLapply(cl,file.list,function(fname){
+            track=.readDiaSessions(file=fname,ab.track=ab.track, censorSingle = censorSingle, frameRecord = frameRecord)
+            # add indexPerTrackll to track name
+            indexPerTrackll=1:length(track)
+            names(track)=mapply(paste,names(track),indexPerTrackll,sep=".")
+            return(track)
+        })
+        
+        # stop cluster
+        cat("Stopping clusters...\n")
+        parallel::stopCluster(cl)
+        
+        names(trackll)=file.name
+        # names(track)=file.name
+        
     }
     
-    #Write the data frame df into the .csv and display confirmation text
-    write.csv(df, file="outputCol.csv");
-    cat("outputCol.csv placed in home directory.\n\n");
+    # cleaning tracks by image mask
+    #if (mask==T){
+    #    trackll=maskTracks(trackll=trackll,maskl=mask.list)
+    #}
+    
+    # merge masked tracks
+    # merge has to be done after mask
+    
+    
+    # if (merge==T){
+    #     for (i in 1:length(file.list)){
+    #         trackll[[i]]=track[[i]]
+    #         names(trackll)[i]=file.name[i]
+    #     }
+    # }
+    
+    
+    # trackll naming scheme
+    # if merge==F, list takes the name of individual file name within folder
+    # file.name > data.frame.name
+    # if merge==T, list takes the folder name
+    # folder.name > data.frame.name
+    
+    # filtration by image mask
+    #if (mask==T){
+    #    trackll=maskTracks(trackll=trackll,maskl=mask.list)
+    #}
+    
+    # merge masked tracks
+    # merge has to be done after mask
+    
+    #if (merge==T){
+        
+        # trackll naming scheme
+        # if merge==F, list takes the name of individual file name within folder
+        # file.name > data.frame.name
+        # if merge==T, list takes the folder name
+        # folder.name > data.frame.name
+        
+        # concatenate track list into one list of data.frames
+        #for (i in 1:length(file.list)){
+        #    track.holder=c(track.holder,trackll[[i]])
+        #}
+        
+        # rename indexPerTrackll of index
+        # extrac index
+        #Index=strsplit(names(track.holder),split="[.]")  # split="\\."
+        
+        # remove the last old indexPerTrackll
+        #Index=lapply(Index,function(x){
+            #x=x[1:(length(x)-1)]
+            #x=paste(x,collapse=".")})
+        
+        # add indexPerTrackll to track name
+        #indexPerTrackll=1:length(track.holder)
+        #names(track.holder)=mapply(paste,Index,
+                                   #indexPerTrackll,sep=".")
+        
+        # make the result a list of list with length 1
+        #trackll=list()
+        #trackll[[1]]=track.holder
+        #names(trackll)[[1]]=folder.name
+        
+        # trackll=track.holder
+    #}
+    
+    #     }else{
+    #
+    #         # list of list of data.frames,
+    #         # first level list of folder names and
+    #         # second level list of data.frames
+    #
+    #         for (i in 1:length(file.list)){
+    #
+    #             track=.readDiatrack(file=file.list[i],ab.track=ab.track)
+    #             # concatenate tracks into one list of data.frames
+    #             track.holder=c(track.holder,track)
+    #
+    #         }
+    #
+    #         # add indexPerTrackll to track name
+    #         indexPerTrackll=1:length(track.holder)
+    #
+    #         names(track.holder)=mapply(paste,names(track.holder),
+    #                                    indexPerTrackll,sep=".")
+    #
+    #         # make the result a list of list with length 1
+    #         trackll[[1]]=track.holder
+    #         names(trackll)[[1]]=folder.name
+    #
+    
+    #
+    #
+    #         if (mask==T){
+    #             trackll=maskTracks(trackll,mask.list)
+    #         }
+    #
+    #     }
+    
+    return(trackll)
 }
